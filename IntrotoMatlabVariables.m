d83819267397c78ac5ff10f11d9b5b580802b947
rustturf <rustturf@gmail.com>
2026-01-18 22:12:34 +0545

    Introduction to Variables in Matlab

---
+++ IntrotoMatlabVariables.m
@@ -0,0 +1,8 @@
+%This is the code for Intro to Matlab Variables. I will explain how to create and use variables in Matlab.
+%Variables are used to store data values. In Matlab, you can create a variable by simply assigning a value to a name.
+%For Example: In order to create a variable named 'x' and assign it the value of 10, you would write the following code:
+x=10;
+%Note that the semicolon at the end of the line suppresses the output in the command window.
+%You can also create variables to store different types of data, such as strings, arrays, and matrices.
+%For Example: To create a string variable named 'name' and assign it the value 'John', you would write:
+name='John';
